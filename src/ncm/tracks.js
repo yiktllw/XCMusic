4def2dee2fff0cd78e59f4ca1e699ca12f1af4c2
yiktllw <151817116+yiktllw@users.noreply.github.com>
2024-09-11 19:55:46 +0800

    Update tracks.js

--- src/ncm/tracks.js
+++ src/ncm/tracks.js
@@ -1,10 +1,39 @@
 
 export class Tracks{
     constructor({
-        url = '/tracks/all',
+        url = 'playlist/tracks/all',
         tracks = [],
     }){
         this._url = url;
         this._tracks = tracks;
+    }
+}
+
+export class Track{
+    constructor({
+        url = '/playlist/tracks/all',
+        track = null,
+    }){
+        this._track = {
+            id: 0,
+            name: '',
+            tns: '',
+            al: {
+                picUrl: '',
+            },
+            ar: [
+                {
+                },
+            ];
+            _picUrl: '',
+        }
+        if (track !== null && url === '/playlist/tracks/all') {
+            this._track.id = track.id;
+            this._track.name = track.name;
+            this._track.tns = track.tns[0] ?? '';
+            this._track.al.picUrl = track.al.picUrl;
+            this._track._picUrl = track.al.picUrl + '?param=40y40';
+            
+        }
     }
 }
