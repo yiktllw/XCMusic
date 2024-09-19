ac91129936cdf1b612471f9206aa6a8c92ee47bb
yiktllw <151817116+yiktllw@users.noreply.github.com>
2024-09-20 02:44:44 +0800

    解决css动画造成的内存泄漏(使用svg动画代替)

--- src/components/YPlaying.vue
+++ src/components/YPlaying.vue
@@ -1,8 +1,6 @@
 <template>
     <div class="container">
-        <div class="bar bar1"></div>
-        <div class="bar bar2"></div>
-        <div class="bar bar3"></div>
+        <img src="@/assets/playing.svg" width="15" height="15">
     </div>
 </template>
 
@@ -19,43 +17,5 @@ export default {
     width: 15px;
     height: 15px;
     align-items: flex-end;
-}
-
-.bar {
-    width: 2.0px;
-    margin:1.5px;
-    border-top-left-radius: 1px;
-    border-top-right-radius: 1px;
-    /* Width of the bars */
-    background-color: rgb(234, 78, 68);
-    animation: sineWave 1s infinite;
-}
-
-.bar1 {
-    animation-delay: 0s;
-}
-
-.bar2 {
-    animation-delay: -0.25s;
-    /* Phase shift of pi/2 */
-}
-
-.bar3 {
-    animation-delay: -.5s;
-    /* Phase shift of pi */
-}
-
-@keyframes sineWave {
-    0% {
-        height: 20%;
-    }
-
-    50% {
-        height: 100%;
-    }
-
-    100% {
-        height: 20%;
-    }
 }
 </style>
