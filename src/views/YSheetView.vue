06230983b1517365daa4987bd0036f78337b3d54
yiktllw <151817116+yiktllw@users.noreply.github.com>
2024-10-18 19:54:10 +0800

    refactor: 固定electron-store版本

---
+++ src/views/YSheetView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useStore } from 'vuex';
+
+export default defineComponent({
+    name: 'YSheetView',
+    setup() {
+        const store = useStore();
+        return {
+            login: store.state.login,
+        };
+    },
+});
+
+</script>
