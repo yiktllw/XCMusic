9d03bf051928ed803264542d903da8c0daf56cb7
yiktllw <151817116+yiktllw@users.noreply.github.com>
2024-10-19 14:19:48 +0800

    fix: 修复网页端下载功能问题

---
+++ src/views/YMarkdownView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { marked } from 'marked';
+
+export default defineComponent({
+    name: 'YMarkdownView',
+    props: {
+        filePath: {
+            type: String,
+            required: true,
+        },
+    },
+    computed: {
+        markdownContent() {
+            return marked.parse(this.filePath);
+        },
+    },
+    setup(props) {
+        
+    },
+});
+</script>
